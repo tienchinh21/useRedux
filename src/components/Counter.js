6601f2534e0fd768cfdf1d3c8817f9e78d8b6a9f
tienchinh21 <tienchinh21.it@gmail.com>
2023-10-15 19:55:23 +0700

    use-redux

---
+++ src/components/Counter.js
@@ -0,0 +1,39 @@
+import { Component } from 'react';
+import { useSelector, useDispatch, connect } from 'react-redux';
+import { counterActions } from '../store/counter';
+
+import classes from './Counter.module.css';
+
+const Counter = () => {
+
+    const dispatch = useDispatch();
+    const counter = useSelector(state => state.counter.counter);
+    const show = useSelector(state => state.counter.showCounter);
+
+    const incrementHandler = () => {
+        dispatch(counterActions.increment());
+    }
+    const increaseHandler = () => {
+        dispatch(counterActions.increase(6));
+    }
+    const decrementHandler = () => {
+        dispatch(counterActions.decrement())
+    }
+
+    const toggleCounterHandler = () => {
+        dispatch(counterActions.toggleCounter())
+    };
+
+    return (
+        <main className={classes.counter}>
+            <h1>Redux Counter</h1>
+            {show && <div className={classes.value}>{counter}</div>}
+            <button onClick={incrementHandler}>increment</button>
+            <button onClick={increaseHandler}>increment 5</button>
+            <button onClick={decrementHandler}>decrement</button>
+            <button onClick={toggleCounterHandler}>Toggle Counter</button>
+        </main>
+    );
+};
+
+export default Counter;
